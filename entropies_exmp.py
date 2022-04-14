d9a34c9f6162bba372bab5e03088ff5b238e6552
enricava <43634392+enricava@users.noreply.github.com>
2022-04-14 20:50:17 +0200

    Example files for figures

---
+++ entropies_exmp.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+wordfile = 'allowed_words.txt'
+savefile = 'datafile.npy'
+words = np.loadtxt(wordfile, dtype = 'str')
+
+with open(savefile,'rb') as f:
+    matrix1=np.load(f)
+    matrix2=np.load(f)
+
+def safelog2(x):
+    if x == 0: 
+        return 0
+    
+    return np.log2(x)
+
+d = {}
+for i in range(len(words)):
+    e = 0
+    for f in matrix2[i]/12972:
+        e -= f * safelog2(f)
+    d[words[i]] = e
+
+entropies = []
+for w in sorted(d, key=d.get, reverse=True)[:10]:
+    entropies.append('\\texttt{'+w.upper()+'}' + ' & '+ str(d[w]) + '\\\\')
+
+for elem in entropies[:10]:
+    print( elem)
---
+++ entropyblack_exmp.py
@@ -0,0 +1,22 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+wordfile = 'allowed_words.txt'
+savefile = 'datafile.npy'
+words = np.loadtxt(wordfile, dtype = 'str')
+
+with open(savefile,'rb') as f:
+    matrix1=np.load(f)
+    matrix2=np.load(f)
+
+def safelog2(x):
+    if x == 0: 
+        return 0
+    
+    return np.log2(x)
+
+e = 0
+for f in matrix2[1134]/12972:
+    e -= f * safelog2(f)
+
+print(e)
