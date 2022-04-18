48b144d3f04505038fbb8dcb8a7a0563dd22e3ed
enricava <43634392+enricava@users.noreply.github.com>
2022-04-18 17:30:53 +0200

    Update efficient_precomputation.py

--- efficient_precomputation.py
+++ efficient_precomputation.py
@@ -3,47 +3,55 @@ from tqdm import tqdm
 from itertools import product
 from matplotlib import pyplot as plt
 
-
 wordfile = 'allowed_words.txt'
 savefile = 'datafile.npy'
-solutonfile = 'solutions.txt'
 
 # Load allowed words into array
-words = np.loadtxt(wordfile, dtype = 'str')
-solutions = np.loadtxt(solutonfile, dtype = 'str')
+words1 = np.loadtxt(wordfile, dtype = 'str')
+words2 = words1
 
 # Generates pattern matrix using words1 as solutions and words2 as inputs
-def pattern_matrix(words1, words2):
 
-    list1 = np.array([[ord(c) for c in w] for w in words1], dtype=np.uint8)
-    list2 = np.array([[ord(c) for c in w] for w in words2], dtype=np.uint8)
+list1 = np.array([[ord(c) for c in w] for w in words1], dtype=np.uint8)
+list2 = np.array([[ord(c) for c in w] for w in words2], dtype=np.uint8)
 
-    l1 = len(list1)
-    l2 = len(list2)
+l1 = len(list1)
+l2 = len(list2)
 
-    # First we calculate every possible combination of letters
-    # equalities[a,b,i,j] = words1[a][i] == words2[b][j]
-    equalities = np.zeros((l1, l2, 5, 5), dtype=bool)
-    for i, j in product(range(5), range(5)):
-        equalities[:, :, i, j] = np.equal.outer(list1[:, i], list2[:, j])
+print('Comparing words...')
+# First we calculate every possible combination of letters
+# equalities[a,b,i,j] = words1[a][i] == words2[b][j]
+equalities = np.zeros((l1, l2, 5, 5), dtype=bool)
+for i, j in tqdm(product(range(5), range(5))):
+    equalities[:, :, i, j] = np.equal.outer(list1[:, i], list2[:, j])
 
-    matrix = np.zeros((l1,l2), dtype=np.uint8)
+matrix = np.zeros((l1,l2), dtype=np.uint8)  # word x word : pattern
+entropies = np.zeros(l1)                    # word : pattern
 
-    for i in range(5):
-        # matches[a, b] : words1[a][i] == words2[b][i]
-        matches = equalities[:, :, i, i].flatten()
-        matrix.flat[matches] += 2 * 3**(4-i)
+print('Green pass...')
+for i in tqdm(range(5)):
+    # matches[a, b] : words1[a][i] == words2[b][i]
+    matches = equalities[:, :, i, i].flatten()
+    matrix.flat[matches] += 2 * 3**(4-i)
+    for k in range(5):
+        # Avoid amber pass
+        equalities[:, :, k, i].flat[matches] = False
+        equalities[:, :, i, k].flat[matches] = False
+
+print('Amber pass...')
+for i, j in tqdm(product(range(5), range(5))):
+        matches = equalities[:, :, i, j].flatten()
+        matrix.flat[matches] += 3**(4-i)
+        #Avoid next passes
         for k in range(5):
-            # Avoid amber pass
-            equalities[:, :, k, i].flat[matches] = False
+            equalities[:, :, k, j].flat[matches] = False
             equalities[:, :, i, k].flat[matches] = False
 
-    for i, j in product(range(5), range(5)):
-            matches = equalities[:, :, i, j].flatten()
-            matrix.flat[matches] += 3**(4-i)
-            #Avoid next passes
-            for k in range(5):
-                equalities[:, :, k, j].flat[matches] = False
-                equalities[:, :, i, k].flat[matches] = False
+print('Calculating entropies...')
+for col in tqdm(matrix.T):
+    ps, counts = np.unique(col, return_counts=True)
+    entropies[col] = - np.dot(counts/len(ps), np.log2(counts/len(ps)))
 
-    return matrix
+with open(savefile, 'wb') as f:
+    np.save(f, matrix)
+    np.save(f, entropies)
