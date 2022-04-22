bb11c0b2a100f8e36595c02b75ee7dbba1f3ac34
angeben <95167445+angeben@users.noreply.github.com>
2022-04-22 20:29:24 +0200

    Add files via upload

---
+++ GeneticAlgorithm/WordleGenetic_final_ahorasiquesi_maybe.py
@@ -0,0 +1,173 @@
+import random as rnd
+import numpy as np
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+wordfile = 'allowed_words.txt'
+solutionfile = 'solutions.txt'
+datafile = 'datafile.npy'
+
+# Load allowed words into array
+words = np.loadtxt(wordfile, dtype = 'str')
+solutions = np.loadtxt(solutionfile, dtype = 'str')
+
+# Load data matrices
+with open(datafile,'rb') as f:
+    pattern_matrix=np.load(f)      # word x word : pattern
+    entropies=np.load(f)      # word : entropy
+
+# Get word position in 'words'
+def get_word_position(word): # O(logn)
+    return words.searchsorted(word)
+
+# Get pattern between words as list
+# Pattern representation is in ternary base
+# 0 : gray
+# 1 : yellow
+# 2 : green
+def get_pattern(word1_position, word2_position):
+    pattern = pattern_matrix[word1_position, word2_position]
+    list_pattern = np.zeros(5, dtype=np.uint8)
+    for i in range(5):
+        if pattern % 3 == 1:
+            list_pattern[4-i] = 1
+        elif pattern % 3 == 2:
+            list_pattern[4-i] = 2
+        pattern = pattern // 3
+    return list_pattern
+
+# Get entropy of a word
+def get_entropy(word_position):
+    return entropies[word_position]
+
+#--------------------------------------------------------------
+#                    GENETIC ALGORITHM
+#--------------------------------------------------------------
+
+def init(N):
+    gen = set()
+    while len(gen) != N:
+        index = rnd.randrange(len(words))
+        gen.add(words[index])
+    return list(gen)
+
+def fitness(word, best, pattern, gray):
+    f = get_entropy(get_word_position(word))
+    if best != '-----': # Not the first attempt
+        for i in range(5):
+            if pattern[i] == 2 and best[i] == word[i]:
+                f += 5
+            elif pattern[i] == 1 and best[i] != word[i] and best[i] in word:
+                f += 2
+            if word[i] in gray:
+                f -= 2
+    return f
+
+def selection(population, best, pattern, gray):
+    bestfit = 0
+    for w in population:
+        fit = fitness(w,best, pattern, gray)
+        if fit > bestfit:
+            bestfit = fit
+            bestword = w
+    return bestword
+
+def mutation(child, maxTries = 100):
+    found = False
+    tries = 0    
+    while not found and tries < maxTries:
+        newchild = ""
+        i = rnd.randint(0,4)
+        l = chr(rnd.randrange(ord('a'), ord('z')))
+        for j in range(5):
+            if j == i:
+                newchild += l
+            else:
+                newchild += child[j]
+        k = get_word_position(newchild)
+        found = k < len(words) and newchild != child and words[k] == newchild
+        tries += 1
+    if found:
+        return newchild
+    else:
+        return child
+
+def crossover(guessed, colors, attempts):
+    found = False
+    while not found:
+        k = rnd.randint(0,len(words)-1)
+        word = words[k]
+        found = not word in attempts
+        for i in range(5):
+            if colors[i] == 2:
+                found = found and word[i] == guessed[i]
+            elif colors[i] == 1:
+                found = found and (word[i] != guessed[i] and guessed[i] in word)
+            else:
+                found = found and (not guessed[i] in word)
+    return word
+
+def attempt(best, solution, guessed, colors, gray):
+    newpattern = get_pattern(get_word_position(best), get_word_position(solution))
+    for i in range(5):
+        if newpattern[i] > colors[i]:
+            colors[i] = newpattern[i]
+            guessed[i] = best[i]
+        if newpattern[i] == 0:
+            gray.add(best[i])
+    return guessed, colors, gray
+
+# Genetic Algorithm to solve Wordle
+# N: size of the generation
+# mutProb: probability for mutation
+# solution: solution for the game
+def geneticAlgorithm(N, mutProb, solution):    
+    colors = [0,0,0,0,0]
+    guessed = ['-','-','-','-','-',]
+    gray = set()
+    attempts = []
+    found = False
+
+    population = init(N)
+
+    while not found:
+        best = selection(population, guessed, colors, gray) 
+        guessed, colors, gray = attempt(best, solution, guessed, colors, gray)
+        attempts.append(best)
+
+        if colors == [2,2,2,2,2]:
+            found = True
+            break
+       
+        nextGen = []
+        for i in range(N):
+            child = crossover(guessed, colors, attempts)
+            r = rnd.uniform(0,1)
+            if r < mutProb:
+                child = mutation(child)
+            nextGen.append(child)
+        population = nextGen
+
+    return attempts
+
+# Solves Wordle 'iterations' times with 'word' as the solution
+def simulate(word, iterations, N, mutationProb):
+    results = [0 for _ in range(8)]
+    for _ in tqdm(range(iterations)):
+        sol = geneticAlgorithm(N, mutationProb, word)
+        results[len(sol)-1] += 1
+    print(results)
+    score = 0
+    for i in range(len(results)):
+        score += (i+1)*results[i]
+    score /= iterations
+    print('Score = ' + str(score))
+    plt.title('Simulation results for the word ' + str(word))
+    plt.ylabel('Frequency')
+    plt.xlabel('Score')
+    plt.bar(range(1,9), results)
+    plt.savefig('simGene.png', dpi=1000, transparent=True)
+    plt.show()
+
+
+simulate('merry', 10, 250, 0.1)
