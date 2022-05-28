050bbca6f65bad2c049016fc8cf3c4507c7bf463
angeben <95167445+angeben@users.noreply.github.com>
2022-05-28 18:35:52 +0200

    Update Simulatorv1.py

--- InformationTheoryAlgorithm/Simulatorv1.py
+++ InformationTheoryAlgorithm/Simulatorv1.py
@@ -151,7 +151,7 @@ def simulations(mode, setstarter = None):
 
     print('Results =',results)
     print('Score =', score)
-    plt.title('Simulation results '+ mode + starter)
+    plt.title('Simulation results '+ mode + ' ' + starter)
     plt.suptitle(str(results) + ' ' + str(score))
     plt.ylabel('Frequency')
     plt.xlabel('Score')
